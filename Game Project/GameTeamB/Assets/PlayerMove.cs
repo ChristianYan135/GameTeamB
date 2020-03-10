a07ad8543d8827dd3ffb2fe1cb58f7868c30c274
Grape In Need Of Surgery <45608780+FrankBartkus@users.noreply.github.com>
2020-03-10 16:18:13 -0700

    push

---
+++ Game Project/GameTeamB/Assets/PlayerMove.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed;
+    public float dashSpeed;
+    public KeyCode up;
+    public KeyCode down;
+    public KeyCode left;
+    public KeyCode right;
+    public KeyCode boost;
+    public bool dashing;
+    Vector2 direction = new Vector2(0, 0);
+    Rigidbody2D r;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        r = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetDirection();
+    }
+    void FixedUpdate()
+    {
+        Move();
+    }
+    void GetDirection()
+    {
+        direction = new Vector2(0, 0);
+        if (Input.GetKey(up))
+        {
+            direction += Vector2.up;
+        }
+        if (Input.GetKey(down))
+        {
+            direction += Vector2.down;
+        }
+        if (Input.GetKey(left))
+        {
+            direction += Vector2.left;
+        }
+        if (Input.GetKey(right))
+        {
+            direction += Vector2.right;
+        }
+        direction.Normalize();
+    }
+    void Move()
+    {
+        r.velocity = direction * speed;
+    }
+    void Boosting()
+    {
+        if(Input.GetKeyDown(boost))
+        {
+            r.AddForce(direction * dashSpeed);
+        }
+    }
+}
