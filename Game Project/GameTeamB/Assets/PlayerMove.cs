1154553f1570e5b53448a19f4a1bbf946f90da78
Frank Bartkus <frank.bartkus@dit1944us.DIGIPEN.EDU>
2020-03-10 17:15:21 -0700

    Comit

--- Game Project/GameTeamB/Assets/PlayerMove.cs
+++ Game Project/GameTeamB/Assets/PlayerMove.cs
@@ -6,60 +6,80 @@ public class PlayerMove : MonoBehaviour
 {
     public float speed;
     public float dashSpeed;
-    public KeyCode up;
-    public KeyCode down;
-    public KeyCode left;
-    public KeyCode right;
-    public KeyCode boost;
+    public KeyCode upKey;
+    public KeyCode downKey;
+    public KeyCode leftKey;
+    public KeyCode rightKey;
+    public KeyCode boostKey;
     public bool dashing;
+    public float dashTimerMax;
+    float dashTimer;
     Vector2 direction = new Vector2(0, 0);
     Rigidbody2D r;
-
+    Vector2 up;
+    Vector2 down;
+    Vector2 left;
+    Vector2 right;
+    bool pressing;
     // Start is called before the first frame update
     void Start()
     {
         r = GetComponent<Rigidbody2D>();
+        up = Vector2.up;
+        down = Vector2.down;
+        left = Vector2.left;
+        right = Vector2.right;
+        dashTimer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         GetDirection();
-    }
-    void FixedUpdate()
-    {
         Move();
+        Boosting();
     }
     void GetDirection()
     {
-        direction = new Vector2(0, 0);
-        if (Input.GetKey(up))
+        pressing = false;
+        if (Input.GetKey(upKey))
         {
-            direction += Vector2.up;
+            direction += up;
+            pressing = true;
         }
-        if (Input.GetKey(down))
+        if (Input.GetKey(downKey))
         {
-            direction += Vector2.down;
+            direction += down;
+            pressing = true;
         }
-        if (Input.GetKey(left))
+        if (Input.GetKey(leftKey))
         {
-            direction += Vector2.left;
+            direction += left;
+            pressing = true;
         }
-        if (Input.GetKey(right))
+        if (Input.GetKey(rightKey))
         {
-            direction += Vector2.right;
+            direction += right;
+            pressing = true;
         }
+        transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90.0f);
         direction.Normalize();
+        Debug.Log(transform.eulerAngles);
     }
     void Move()
     {
-        r.velocity = direction * speed;
+        if (pressing)
+            if(r.velocity.magnitude < speed)
+               r.velocity = direction * speed;
     }
     void Boosting()
     {
-        if(Input.GetKeyDown(boost))
+        if(Input.GetKeyDown(boostKey) && dashTimer == dashTimerMax)
         {
-            r.AddForce(direction * dashSpeed);
+            r.AddForce(direction * dashSpeed / Time.deltaTime);
+            dashTimer = 0;
         }
+        if (dashTimer < dashTimerMax)
+            dashTimer = Mathf.Clamp(dashTimer + Time.deltaTime, 0.0f, dashTimerMax);
     }
 }
