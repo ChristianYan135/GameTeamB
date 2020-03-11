426fe4d8826ef460312f6f6a3dbcf45b4ec1fa8c
ChristianYan135 <48737820+ChristianYan135@users.noreply.github.com>
2020-03-11 16:20:51 -0700

    Worked on ball script and added two players

--- Game Project/GameTeamB/Assets/Scripts/Ball.cs
+++ Game Project/GameTeamB/Assets/Scripts/Ball.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    public float startSpeed = 1.0f;
+    Vector2 startSpeed = new Vector2(1.0f,1.0f);
 
     public float speedM = 1.0f;
 
+    int timesHit = 1;
+
+    int turn = 0;
     Rigidbody2D rigidbody2d;
 
     SpriteRenderer renderer;
@@ -17,31 +20,47 @@ public class Ball : MonoBehaviour
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         renderer = GetComponent<SpriteRenderer>();
-        rigidbody2d.velocity = Vector2.right * startSpeed;
+        renderer.color = Color.grey;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        Debug.Log(rigidbody2d.velocity);
     }
+
     void OnCollisionEnter2D(Collision2D col)
     {
-        if(col.collider.gameObject.layer == LayerMask.NameToLayer("Wall"))
-        {
-            
-        }
         if (col.collider.gameObject.layer == LayerMask.NameToLayer("Player1"))
         {
-            rigidbody2d.velocity *= speedM;
-            transform.rotation = col.collider.gameObject.transform.rotation;
-            renderer.color = Color.blue;
+            if (col.collider.gameObject.GetComponent<PlayerMove>().dashing)
+            {
+                rigidbody2d.velocity = startSpeed * (speedM * timesHit);
+                transform.rotation = col.collider.gameObject.transform.rotation;
+                renderer.color = Color.blue;
+                timesHit++;
+                turn = 1;
+            }
+            else if(!col.collider.gameObject.GetComponent<PlayerMove>().dashing && turn == 2)
+            {
+                Destroy(col.collider.gameObject);
+            }
+            
         }
         if (col.collider.gameObject.layer == LayerMask.NameToLayer("Player2"))
         {
-            rigidbody2d.velocity *= speedM;
-            transform.rotation = col.collider.gameObject.transform.rotation;
-            renderer.color = Color.red;
+            if (col.collider.gameObject.GetComponent<PlayerMove>().dashing)
+            {
+                rigidbody2d.velocity = startSpeed * (speedM * timesHit);
+                transform.rotation = col.collider.gameObject.transform.rotation;
+                renderer.color = Color.red;
+                timesHit++;
+                turn = 2;
+            }
+            else if (!col.collider.gameObject.GetComponent<PlayerMove>().dashing && turn == 1)
+            {
+                Destroy(col.collider.gameObject);
+            }
         }
     }
 }
